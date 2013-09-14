f8747ce0e7cdef50803bb93db04287d90c467278
Matt George <matt@matt-VirtualBox.(none)>
2013-09-14 18:00:57 -0400

    adding stuff to the new form

---
+++ app/controllers/pics_controller.rb
@@ -0,0 +1,24 @@
+class PicsController < ApplicationController
+	# code that gets run on the new.html.erb
+	def new
+		@pic = Pic.new
+	end
+
+	def index
+	end
+
+	def create
+	end
+
+	def show
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def update
+	end
+end
